8bdc1dac7776f3ba2978c1e9929021e9dc3e5169
BanPei Lim <banpei.lim@gmail.com>
2023-02-27 22:20:09 +0800

    first

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("send_message", () => {
+    socket.emit("receive_message", "This is Lim Ban Pei");
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING");
+});
