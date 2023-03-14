836a14283bc48d813f9eae52823cf3a130ab6651
BanPei Lim <banpei.lim@gmail.com>
2023-03-14 20:44:42 +0800

    first

--- index.js
+++ index.js
@@ -15,9 +15,34 @@ const io = new Server(server, {
   },
 });
 
-app.get("/emit-event", (req, res) => {
-  io.emit("my-event", "Tizen OS");
-  res.send("Event emitted!");
+app.get("/emit-event_1", (req, res) => {
+  io.emit("my-event_1", "Tizen OS");
+  res.send("Event_1 emitted");
+});
+
+app.get("/emit-event_2", (req, res) => {
+  io.emit("my-event_2", "Tizen OS");
+  res.send("Event_2 emitted");
+});
+
+app.get("/emit-event_3", (req, res) => {
+  io.emit("my-event_3", "Tizen OS");
+  res.send("Event_3 emitted");
+});
+
+app.get("/emit-event_4", (req, res) => {
+  io.emit("my-event_4", "Tizen OS");
+  res.send("Event_4 emitted");
+});
+
+app.get("/emit-event_5", (req, res) => {
+  io.emit("my-event_5", "Tizen OS");
+  res.send("Event_5 emitted");
+});
+
+app.get("/emit-event_6", (req, res) => {
+  io.emit("my-event_6", "Tizen OS");
+  res.send("Event_6 emitted");
 });
 
 io.on("connection", (socket) => {
