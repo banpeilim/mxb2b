bd3928a32ef59737c9fd273054e9be505134835f
BanPei Lim <banpei.lim@gmail.com>
2025-02-08 08:30:34 +0800

    second

--- index.js
+++ index.js
@@ -15,40 +15,13 @@ const io = new Server(server, {
   },
 });
 
-app.get("/emit-event_1", (req, res) => {
-  io.emit("my-event_1", "Tizen OS");
-  res.send("Event_1 emitted");
-});
-
-app.get("/emit-event_2", (req, res) => {
-  io.emit("my-event_2", "Tizen OS");
-  res.send("Event_2 emitted");
-});
-
-app.get("/emit-event_3", (req, res) => {
-  io.emit("my-event_3", "Tizen OS");
-  res.send("Event_3 emitted");
-});
-
-app.get("/emit-event_4", (req, res) => {
-  io.emit("my-event_4", "Tizen OS");
-  res.send("Event_4 emitted");
-});
-
-app.get("/emit-event_5", (req, res) => {
-  io.emit("my-event_5", "Tizen OS");
-  res.send("Event_5 emitted");
-});
-
-app.get("/emit-event_6", (req, res) => {
-  io.emit("my-event_6", "Tizen OS");
-  res.send("Event_6 emitted");
-});
-
-app.get("/emit-event_7", (req, res) => {
-  io.emit("my-event_7", "Tizen OS");
-  res.send("Event_7 emitted");
-});
+// Dynamically create 50 event-emitting routes
+for (let i = 1; i <= 50; i++) {
+  app.get(`/emit-event_${i}`, (req, res) => {
+    io.emit(`my-event_${i}`, "Tizen OS");
+    res.send(`Event_${i} emitted`);
+  });
+}
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
